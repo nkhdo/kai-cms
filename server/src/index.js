419e70894ac5279ad5788f8c168570c4acc290f2
Kai Do <me@kaid.sh>
2019-02-22 20:22:29 +0700

    Simple server

---
+++ server/src/index.js
@@ -0,0 +1,14 @@
+const fastify = require('fastify')({ logger: true });
+
+fastify.get('/', async () => ({ hello: 'world' }));
+
+const start = async () => {
+  try {
+    await fastify.listen(3000);
+    fastify.log.info(`server listening on ${fastify.server.address().port}`);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
