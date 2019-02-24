a0a7a6591f5839f3f2ee7feac18037a361917809
Kai Do <me@kaid.sh>
2019-02-24 23:35:36 +0700

    init manager UI

---
+++ manager/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <div id="sidebar">
+      <sidebar/>
+    </div>
+    <div id="main">
+      <div id="main-menu">
+        Menu
+      </div>
+      <div id="main-container">
+        <router-view/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Sidebar from '@/components/Sidebar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Sidebar
+  }
+}
+</script>
+
+
+<style lang="scss">
+html, body, #app {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+#app {
+  display: flex;
+  #sidebar {
+    width: 250px;
+    border-right: 1px solid #f0f0f0;
+  }
+  #main {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    &-container {
+      flex: 1;
+      overflow: auto;
+    }
+  }
+}
+</style>
---
+++ manager/src/components/Sidebar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="kd-sidebar">
+    <div class="kd-sidebar-header">
+      <vs-avatar text="KaiCMS"/> KaiCMS
+    </div>
+    <div class="kd-sidebar-body">
+      <vs-list>
+        <vs-list-header icon="supervisor_account" title="Group 1"></vs-list-header>
+        <vs-list-item icon="check" title="Snickerdoodle" subtitle="An excellent companion"></vs-list-item>
+        <vs-list-item icon="check" title="Sapporo Haru" subtitle="An excellent polish restaurant, quick delivery and hearty, filling meals"></vs-list-item>
+        <vs-list-header icon="how_to_reg" title="Group 2" color="success"></vs-list-header>
+        <vs-list-item icon="verified_user" title="Enid's" subtitle="At night a bar, during the day a delicious brunch spot."></vs-list-item>
+        <vs-list-item icon="verified_user" title="Veronika Ossi" subtitle="Has not watched anything recently"></vs-list-item>
+      </vs-list>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Sidebar'
+}
+</script>
+
+<style scoped lang="scss">
+.kd-sidebar {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  &-header {
+    padding: 5px 10px;
+  }
+  &-body {
+    flex: 1;
+    overflow: auto;
+  }
+}
+</style>
